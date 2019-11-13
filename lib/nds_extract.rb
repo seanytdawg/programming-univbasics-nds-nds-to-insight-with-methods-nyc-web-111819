8899f21f8750841e579a065ca09bbc8064401b3e
Sean Tarzy <seantarzy@gmail.com>
2019-11-13 04:59:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,5 +13,10 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  gross = 0 
+  row_index = 0 
+  collumn_index = 0 
+  
+  
   director_data
 end
