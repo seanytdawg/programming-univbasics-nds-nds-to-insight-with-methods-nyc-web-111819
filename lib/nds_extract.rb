fb580f8033b3dbea17a8c760b31b22364cb316de
Sean Tarzy <seantarzy@gmail.com>
2019-11-13 05:03:57 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,4 +13,13 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  gross = 0 
+  row_index = 0 
+  collumn_index = 0 
+  while row_index = collumn_index
+  do 
+gross += director_data[row_index][collumn_index][:gross]
+row_index += 1 
+collumn_index += 1 
+return gross
 end
